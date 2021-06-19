896b67e4b56c1b035a92e83e0ec0b4a165c21b75
Shubham Sanjeev Mayekar <shubham.m@techtreeit.com>
2021-06-20 02:56:49 +0530

    error handling completed

--- app.js
+++ app.js
@@ -12,4 +12,20 @@ app.use(morgan('dev'));
 app.use('/products',productRouters);
 app.use('/orders',ordersRouters);
 
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message,
+            status: error.status
+        }
+    });
+});
+
 module.exports = app;
